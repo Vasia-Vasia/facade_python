faa3f35507916751bb6d4392ade43cba7546bebd
Vasia <34076640+Vasia-Vasia@users.noreply.github.com>
2025-04-30 19:54:33 +0300

    Add files via upload

---
+++ solution/__init__.py
@@ -0,0 +1,12 @@
+from solution.numbers1 import mul1 as mul
+from solution.numbers2 import power2 as power, add2 as add, sub2 as sub
+from solution.numbers3 import sqrt3 as sqrt
+
+
+__all__ = (
+    'mul',
+    'power',
+    'add',
+    'sub',
+    'sqrt'
+)
